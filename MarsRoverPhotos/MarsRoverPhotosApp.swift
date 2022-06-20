96fa3d5cf724e3706ca1e9211845c11dace9c15c
Can Bi <bica@mef.edu.tr>
2022-06-20 16:00:42 +0300

    Initial Commit

---
+++ MarsRoverPhotos/MarsRoverPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsRoverPhotosApp.swift
+//  MarsRoverPhotos
+//
+//  Created by Can Bi on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MarsRoverPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
